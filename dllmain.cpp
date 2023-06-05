13810384d5a31b84db823653e65426e94b0b7381
Cathy J. Fitzpatrick <cathy@cathyjf.com>
2023-06-05 15:19:30 -0700

    Add workarounds relating to ReShade

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,4 +1,5 @@
 #include "pch.h"
+#include <filesystem>
 #include <memory>
 #include <optional>
 #include <string>
@@ -8,14 +9,15 @@
 #include "externals/REFramework/include/reframework/API.hpp"
 #pragma warning(pop)
 
-#define PLUGIN_NAME "REFramework-SpecialK"
-#define LOG_INFO(f, ...) reframework::API::get()->log_info(PLUGIN_NAME ": " f, __VA_ARGS__);
-#define LOG_INFO0(s) LOG_INFO("%s", s)
-#define LOG_ERROR(f, ...) reframework::API::get()->log_error(PLUGIN_NAME ": " f, __VA_ARGS__);
-#define LOG_ERROR0(s) LOG_ERROR("%s", s)
+#include "externals/reshade/include/reshade.hpp"
+
+#include "dxgi.proxydll.h"
 
 namespace {
-HMODULE hThisModule;
+bool bUsingREFramework = false;
+bool bUsingReShade = false;
+bool bTriedLoadingSpecialK = false;
+bool bLoadedSpecialK = false;
 
 class RegKeyCloseGuard {
 public:
@@ -49,36 +51,171 @@ std::optional<std::wstring> GetSpecialKPathFromRegistry() {
     return std::wstring{ buffer };
 }
 
-} // anonymous namespace
+void writeLogMessage(const char *message) {
+    if (bUsingREFramework) {
+        reframework::API::get()->log_info("REFramework-SpecialK: %s", message);
+    }
+    if (bUsingReShade) {
+        reshade::log_message(reshade::log_level::info, message);
+    }
+}
 
-extern "C" __declspec(dllexport) bool reframework_plugin_initialize(const REFrameworkPluginInitializeParam *param) {
-    reframework::API::initialize(param);
+bool executeExe(LPCWSTR filename, WCHAR commandLine[], bool waitForExe = false) {
+    STARTUPINFO startupInfo;
+    ZeroMemory(&startupInfo, sizeof(startupInfo));
+    startupInfo.cb = sizeof(startupInfo);
+
+    PROCESS_INFORMATION processInformation;
+    ZeroMemory(&processInformation, sizeof(processInformation));
+
+    // Note: The string used to hold the command line cannot be const because
+    // the CreateProcess function reserves the right to modify the string.
+    const BOOL bSuccess = CreateProcess(
+            filename,
+            commandLine,
+            NULL,  // lpProcessAttributes
+            NULL,  // lpThreadAttributes
+            FALSE, // bInheritHandles
+            0,     // dwCreationFlags
+            NULL,  // lpEnvironment
+            NULL,  // lpCurrentDirectory
+            &startupInfo,
+            &processInformation);
+    if (!bSuccess) {
+        return false;
+    }
+
+    if (waitForExe) {
+        WaitForSingleObject(processInformation.hProcess, INFINITE);
+    }
+    CloseHandle(processInformation.hProcess);
+    CloseHandle(processInformation.hThread);
+    return true;
+}
+
+bool tryLoadSpecialK(bool bUseLoadLibrary = true) {
+    constexpr LPCWSTR lpMutexName{ L"Local\\REFrameworkSpecialKRunOnceMutex" };
+    auto&& hMutex{ CreateMutex(NULL, TRUE, lpMutexName) };
+    if (!hMutex) {
+        return 1;
+    }
+    if (WaitForSingleObject(hMutex, 0) != WAIT_OBJECT_0) {
+        return 1;
+    }
+
+    if (bTriedLoadingSpecialK) {
+        writeLogMessage("Ignored second attempt to load SpecialK");
+        return bLoadedSpecialK;
+    }
+    bTriedLoadingSpecialK = true;
 
     auto &&skRegistryPath{ GetSpecialKPathFromRegistry() };
     if (!skRegistryPath) {
-        LOG_ERROR0("GetSpecialKPathFromRegistry failed (perhaps Special K is not properly installed?)");
+        writeLogMessage("GetSpecialKPathFromRegistry failed (perhaps Special K is not properly installed?)");
         return false;
     }
 
-    LOG_INFO0("Attempting to load SpecialK...");
-    auto skLoadPath{ *skRegistryPath + L"\\SpecialK64.dll" };
-    auto skModule{ LoadLibrary(skLoadPath.c_str()) };
-    if (!skModule) {
-        LOG_ERROR("%s %s", "Failed to load SpecialK");
+    writeLogMessage("Attempting to load SpecialK...");
+
+    if (bUseLoadLibrary) {
+        auto skLoadPath{ *skRegistryPath + L"\\SpecialK64.dll" };
+        auto skModule{ LoadLibrary(skLoadPath.c_str()) };
+        if (!skModule) {
+            writeLogMessage("Failed to load SpecialK");
+            return false;
+        }
+        writeLogMessage("Successfully loaded SpecialK");
+    } else {
+        auto skLoadPath{ *skRegistryPath + L"\\SKIF.exe" };
+        WCHAR commandLine[]{L"SKIF.exe Start Temp"};
+        if (executeExe(skLoadPath.c_str(), commandLine)) {
+            writeLogMessage("Successfully ran `SKIF Start Temp`");
+        }
+    }
+    bLoadedSpecialK = true;
+    return true;
+}
+
+void spawnSKIFQuitThread() {
+    CreateThread(NULL, 0, [](LPVOID) -> DWORD {
+        Sleep(4 * 1000);
+        auto&& skRegistryPath{ GetSpecialKPathFromRegistry() };
+        if (skRegistryPath) {
+            auto skLoadPath{ *skRegistryPath + L"\\SKIF.exe" };
+            WCHAR commandLine[]{ L"SKIF.exe Quit" };
+            if (executeExe(skLoadPath.c_str(), commandLine)) {
+                writeLogMessage("Successfully ran `SKIF Quit`");
+            }
+        }
+        return 0;
+    }, NULL, 0, NULL);
+}
+
+void spawnSKIFThread(bool bExecuteSkifQuit = true) {
+    CreateThread(NULL, 0, [](LPVOID) -> DWORD {
+        tryLoadSpecialK(false);
+        return 0;
+    }, NULL, 0, NULL);
+}
+
+void unregisterReShadeEvent();
+
+void onReShadePresent(reshade::api::command_queue *, reshade::api::swapchain *, const reshade::api::rect *,
+        const reshade::api::rect *, uint32_t, const reshade::api::rect *) {
+    unregisterReShadeEvent();
+    spawnSKIFQuitThread();
+}
+
+void unregisterReShadeEvent() {
+    reshade::unregister_event<reshade::addon_event::present>(&onReShadePresent);
+}
+
+void doProcessAttach(HMODULE hModule) {
+    DisableThreadLibraryCalls(hModule);
+    const DWORD szBuffer{ MAX_PATH + 1 };
+    WCHAR buffer[szBuffer];
+    const DWORD nSize{ GetModuleFileName(hModule, buffer, szBuffer) };
+    if (nSize != 0) {
+        const std::wstring dllName{ std::filesystem::path{ buffer }.filename() };
+        if (dllName == L"dxgi.dll") {
+            spawnSKIFThread(false);
+        }
+    }
+}
+
+} // anonymous namespace
+
+extern "C" __declspec(dllexport) bool reframework_plugin_initialize(
+        const REFrameworkPluginInitializeParam *param) {
+    bUsingREFramework = true;
+    reframework::API::initialize(param);
+    return tryLoadSpecialK();
+}
+
+extern "C" __declspec(dllexport) bool AddonInit(HMODULE addon_module, HMODULE /*reshade_module*/) {
+    if (!reshade::register_addon(addon_module)) {
         return false;
     }
-    LOG_INFO0("Successfully loaded SpecialK");
+    bUsingReShade = true;
+    reshade::register_event<reshade::addon_event::present>(&onReShadePresent);
+    writeLogMessage("Add-on successfully registered");
     return true;
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID /*lpReserved*/) {
-    hThisModule = hModule;
-
     switch (ul_reason_for_call) {
     case DLL_PROCESS_ATTACH:
+        doProcessAttach(hModule);
+        break;
     case DLL_THREAD_ATTACH:
+        break;
     case DLL_THREAD_DETACH:
+        break;
     case DLL_PROCESS_DETACH:
+        if (bUsingReShade) {
+            unregisterReShadeEvent();
+            reshade::unregister_addon(hModule);
+        }
         break;
     }
     return TRUE;
