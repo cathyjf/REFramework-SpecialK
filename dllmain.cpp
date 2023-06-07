6f7710fb71f6d4b6121064b033113b3d8ac2a506
Cathy J. Fitzpatrick <cathy@cathyjf.com>
2023-06-06 17:44:05 -0700

    Add some features for Yuzu

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,7 +1,9 @@
 #include "pch.h"
 #include <array>
 #include <filesystem>
+#include <fstream>
 #include <memory>
+#include <mutex>
 #include <optional>
 #include <string>
 
@@ -20,6 +22,8 @@
 namespace {
 auto bUsingREFramework = false;
 auto bUsingReShade = false;
+auto bUsingReShadeYuzu = false;
+auto bEnableReShadeYuzuEffects = false;
 
 class RegKeyCloseGuard {
 public:
@@ -58,11 +62,50 @@ void writeLogMessage(const char *message) {
         reframework::API::get()->log_info("REFramework-SpecialK: %s", message);
     }
     if (bUsingReShade) {
-        reshade::log_message(reshade::log_level::info, message);
+        const auto finalMessage{ std::string{ "SpecialK Companion: " } + message };
+        reshade::log_message(reshade::log_level::info, finalMessage.c_str());
+    }
+    static std::mutex mutex;
+    std::scoped_lock lock{ mutex };
+    static auto logStream = ([]() -> std::optional<std::ofstream> {
+        const auto logPath = ([]() -> std::optional<std::wstring> {
+            const auto szBuffer = MAX_PATH + 1;
+            auto buffer{ std::array<WCHAR, szBuffer>{} };
+            const auto nSize = GetModuleFileName(NULL, buffer.data(), szBuffer);
+            if (nSize == 0) [[unlikely]] {
+                return std::nullopt;
+            }
+            return std::filesystem::path{ buffer.data() }
+                .replace_filename(L"SpecialK-Companion.log")
+                .wstring();
+        })();
+        if (!logPath) [[unlikely]] {
+            return std::nullopt;
+        }
+        return std::ofstream{ *logPath };
+    })();
+    if (logStream) {
+        const auto timeString = ([]() -> std::string {
+            const auto time = std::time(nullptr);
+            if (time == -1) [[unlikely]] {
+                return "[time failed] ";
+            }
+            auto localtime = tm{};
+            if (localtime_s(&localtime, &time) != ERROR_SUCCESS) [[unlikely]] {
+                return "[localtime_s failed] ";
+            }
+            auto buffer{ std::array<char, std::size("yyyy-mm-dd hh:mm:ss")>{} };
+            const auto length = std::strftime(buffer.data(), buffer.size(), "%F %T", &localtime);
+            if (length == 0) [[unlikely]] {
+                return "[strftime failed] ";
+            }
+            return "[" + std::string{ buffer.data() } + "] ";
+        })();
+        *logStream << timeString << message << std::endl;
     }
 }
 
-bool executeExe(LPCWSTR filename, WCHAR commandLine[], bool waitForExe = false) {
+bool executeExe(LPCWSTR filename, WCHAR commandLine[], bool waitForExe = false, bool addToJob = false) {
     auto startupInfo{ STARTUPINFO{ .cb{ sizeof(STARTUPINFO) } } };
     auto processInformation{ PROCESS_INFORMATION{} };
 
@@ -86,6 +129,40 @@ bool executeExe(LPCWSTR filename, WCHAR commandLine[], bool waitForExe = false) 
     if (waitForExe) {
         WaitForSingleObject(processInformation.hProcess, INFINITE);
     }
+
+    if (addToJob) {
+#if 0 == 1
+        // This code does not work correctly yet.
+        writeLogMessage("Attempting to create job object...");
+        auto hJob = CreateJobObject(nullptr, nullptr);
+        if (hJob) {
+            writeLogMessage("Successfully created job object");
+            auto limitInfo = JOBOBJECT_EXTENDED_LIMIT_INFORMATION {
+                JOBOBJECT_BASIC_LIMIT_INFORMATION { .LimitFlags {
+                    JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE
+                } }
+            };
+            auto bSetJobInformation = SetInformationJobObject(
+                hJob, JobObjectExtendedLimitInformation, &limitInfo, sizeof(limitInfo));
+            if (!bSetJobInformation) {
+                writeLogMessage("Failed to set job information");
+                CloseHandle(hJob);
+            } else {
+                writeLogMessage("Successfully set job information");
+                auto bAssignProcess = AssignProcessToJobObject(hJob, processInformation.hProcess);
+                if (!bAssignProcess) {
+                    writeLogMessage("Failed to assign child process to job");
+                    CloseHandle(hJob);
+                } else {
+                    // The handle `hJob` is intentionally not closed at this point.
+                    // It will be closed implicitly when this process terminates.
+                    writeLogMessage("Assigned child process to job");
+                }
+            }
+        }
+#endif
+    }
+
     CloseHandle(processInformation.hProcess);
     CloseHandle(processInformation.hThread);
     return true;
@@ -95,10 +172,10 @@ bool tryLoadSpecialK(bool bUseLoadLibrary = true) {
     constexpr auto lpMutexName{ L"Local\\REFrameworkSpecialKRunOnceMutex" };
     const auto &&hMutex{ CreateMutex(NULL, TRUE, lpMutexName) };
     if (!hMutex) {
-        return 1;
+        return true;
     }
     if (WaitForSingleObject(hMutex, 0) != WAIT_OBJECT_0) {
-        return 1;
+        return true;
     }
 
     static auto bLoadedSpecialK = false;
@@ -128,7 +205,7 @@ bool tryLoadSpecialK(bool bUseLoadLibrary = true) {
     } else {
         const auto skLoadPath{ *skRegistryPath + L"\\SKIF.exe" };
         auto commandLine{ std::to_array(L"SKIF.exe Start Temp") };
-        if (executeExe(skLoadPath.c_str(), commandLine.data())) {
+        if (executeExe(skLoadPath.c_str(), commandLine.data(), false, true)) {
             writeLogMessage("Successfully ran `SKIF Start Temp`");
         }
     }
@@ -158,16 +235,47 @@ void spawnSKIFThread(bool bExecuteSkifQuit = true) {
     }, NULL, 0, NULL);
 }
 
-void unregisterReShadeEvent();
-
-void onReShadePresent(reshade::api::command_queue *, reshade::api::swapchain *, const reshade::api::rect *,
+void onReShadePresent(reshade::api::command_queue *,
+        reshade::api::swapchain *, const reshade::api::rect *,
         const reshade::api::rect *, uint32_t, const reshade::api::rect *) {
-    unregisterReShadeEvent();
+    reshade::unregister_event<reshade::addon_event::present>(&onReShadePresent);
     spawnSKIFQuitThread();
 }
 
-void unregisterReShadeEvent() {
-    reshade::unregister_event<reshade::addon_event::present>(&onReShadePresent);
+void onReShadeBeginEffects(reshade::api::effect_runtime *runtime, reshade::api::command_list *,
+        reshade::api::resource_view, reshade::api::resource_view) {
+    reshade::unregister_event<reshade::addon_event::reshade_begin_effects>(&onReShadeBeginEffects);
+    if (bEnableReShadeYuzuEffects) {
+        return;
+    }
+    auto hwnd = reinterpret_cast<HWND>(runtime->get_hwnd());
+    while (hwnd) {
+        const auto szBuffer = 500;
+        auto buffer{ std::array<char, szBuffer>{} };
+        const auto szWindowText = GetWindowTextA(hwnd, buffer.data(), szBuffer);
+        if (szWindowText == 0) {
+            writeLogMessage("Failed to obtain the text in the title bar of the window");
+            break;
+        }
+        const auto windowTitle{ std::string{ buffer.data() } };
+        writeLogMessage(("Window title bar: " + windowTitle).c_str());
+        if (windowTitle.find("The Legend of Zelda: Tears of the Kingdom") != std::string::npos) {
+            bEnableReShadeYuzuEffects = true;
+            break;
+        }
+        hwnd = GetAncestor(hwnd, GA_PARENT);
+        writeLogMessage(static_cast<std::ostringstream &&>((
+            std::ostringstream{} << "Parent hwnd: " << hwnd)).str().c_str());
+    }
+    writeLogMessage(bEnableReShadeYuzuEffects ? "Enabling ReShade effects" : "Disabling ReShade effects");
+    runtime->set_effects_state(bEnableReShadeYuzuEffects);
+}
+
+void onReShadeCreateEffectRuntime(reshade::api::effect_runtime *runtime) {
+    if (!bUsingReShadeYuzu) {
+        return;
+    }
+    reshade::register_event<reshade::addon_event::reshade_begin_effects>(&onReShadeBeginEffects);
 }
 
 void doProcessAttach(HMODULE hModule) {
@@ -199,7 +307,20 @@ extern "C" __declspec(dllexport) bool AddonInit(HMODULE addon_module, HMODULE /*
     }
     bUsingReShade = true;
     reshade::register_event<reshade::addon_event::present>(&onReShadePresent);
+    reshade::register_event<reshade::addon_event::init_effect_runtime>(&onReShadeCreateEffectRuntime);
     writeLogMessage("Add-on successfully registered");
+
+    const auto szBuffer = MAX_PATH + 1;
+    auto buffer{ std::array<WCHAR, szBuffer>{} };
+    const auto nSize = GetModuleFileName(NULL, buffer.data(), szBuffer);
+    if (nSize != 0) {
+        const auto exeName{ std::filesystem::path{ buffer.data() }.filename() };
+        if (exeName == L"yuzu.exe") {
+            bUsingReShadeYuzu = true;
+            writeLogMessage("Detected running of Yuzu with ReShade");
+        }
+    }
+
     return true;
 }
 
@@ -207,7 +328,9 @@ extern "C" __declspec(dllexport) void AddonUninit(HMODULE addon_module, HMODULE 
     if (!bUsingReShade) {
         return;
     }
-    unregisterReShadeEvent();
+    reshade::unregister_event<reshade::addon_event::present>(&onReShadePresent);
+    reshade::unregister_event<reshade::addon_event::init_effect_runtime>(&onReShadeCreateEffectRuntime);
+    reshade::unregister_event<reshade::addon_event::reshade_begin_effects>(&onReShadeBeginEffects);
     reshade::unregister_addon(addon_module);
 }
 
